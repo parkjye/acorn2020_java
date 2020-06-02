540a43583f46539b8bc2b82b328f56bf6aeb420e
parkjye <parkjye44@gmail.com>
2020-06-02 12:45:32 +0900

    Modified Code: MainClass05/test.main/Step05

--- Step05_Method/src/test/main/MainClass05.java
+++ Step05_Method/src/test/main/MainClass05.java
@@ -5,8 +5,7 @@ import java.util.Scanner;
 
 public class MainClass05 {
 	/*
-	 * test.mypac 패키지에 다양한 모양의 메소드를 가지는
-	 * 클래스를 정의하고
+	 * test.mypac 패키지에 다양한 모양의 메소드를 가지는 클래스를 정의하고
 	 * 그 클래스를 이용해서 객체도 생성하고 메소드도 호출하세요.
 	 * */
 	public static void main(String[] args) {

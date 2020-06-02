c97031393f78e4d6326a0cb44e48885ef1fa1698
parkjye <parkjye44@gmail.com>
2020-06-02 12:54:54 +0900

    Created Project: Step07

---
+++ Step07_WrapperClass/src/test/main/MainClass01.java
@@ -0,0 +1,26 @@
+package test.main;
+/*
+ * 	[ 기본 data type의 참조 data type ]
+ * 
+ * 	byte : Byte
+ * 	short : Short
+ *  int : Integer
+ *  long : Long
+ *  
+ *  float : Float
+ *  double : Double
+ *  
+ *  char : Character
+ *  boolean : Boolean
+ *  
+ * */
+public class MainClass01 {
+	public static void main(String[] args) {
+		
+		//기본 데이터
+		int num1 = 10;
+		//참조 데이터
+		Integer num2 = 10;
+		
+	}
+}

f5cccfdb6259ae0d2eae2963e2d7326fc1db6c88
parkjye <64972932+parkjye@users.noreply.github.com>
2020-06-09 19:16:43 +0900

    Delete MainClass01.java

--- Step00_practice/src/test/main/MainClass01.java
+++
@@ -1,9 +0,0 @@
-package test.main;
-import java.util.Random;
-public class MainClass01 {
-	public static void main(String[] args) {
-		Random rNum = new Random();
-		int getNum = rNum.nextInt(45)+1;
-		System.out.println(getNum);
-	}
-}

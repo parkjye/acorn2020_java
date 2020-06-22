b89b5d517efdcdbc0a7a7d84443093a42e043510
parkjye <parkjye44@gmail.com>
2020-06-22 14:41:40 +0900

    주석 수정

--- Step19_JDBC/src/test/main/MainClass01.java
+++ Step19_JDBC/src/test/main/MainClass01.java
@@ -11,12 +11,12 @@ import java.sql.ResultSet;
  * DataBase에 연결해서 SELECT, INSERT, UPDATE, DELETE 작업하기
  * 
  * Oracle에 연결하기 위해서는 드라이버 클래스가 들어있는 ojdbc6.jar 파일을
- * 사용할 수 있도록 설명해야 한다.
+ * 사용할 수 있도록 설정해야 한다.
  *  
  * */
 public class MainClass01 {
 	public static void main(String[] args) {
-		//DB 연결객체를 담을 지역변수 만들기
+		//DB와 연결하는 객체, 연결객체를 담을 지역변수 만들기
 		Connection conn = null;
 		try {
 			//오라클 드라이버 로딩
@@ -25,7 +25,7 @@ public class MainClass01 {
 			//접속할 DB의 정보 @아이피주소:port번호:db이름
 			String url="jdbc:oracle:thin:@localhost:1521:xe";
 			
-			//계정 비밀번호를 이용해서 Connection 객체의 참조값 얻어오기
+			//Connection 생성, 계정 비밀번호를 이용해서 Connection 객체의 참조값 얻어오기
 			conn = DriverManager.getConnection(url, "scott", "tiger");
 			
 			//예외가 발생하지 않고 여기까지 실행순서가 내려오면 접속 성공이다.

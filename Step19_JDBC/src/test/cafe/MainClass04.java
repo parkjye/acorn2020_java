983713709c6c4e7bdaf61e493e02a27d879751a8
parkjye <parkjye44@gmail.com>
2020-06-23 09:55:21 +0900

    JDBC 연습

--- Step19_JDBC/src/test/cafe/MainClass04.java
+++ Step19_JDBC/src/test/cafe/MainClass04.java
@@ -5,45 +5,36 @@ import java.sql.PreparedStatement;
 
 import test.util.DBConnect;
 
-public class MainClass03 {
+public class MainClass04 {
 	public static void main(String[] args) {
 		/*
 		 *  [ cafe: num(pk) / menu / price / cafename ]
 		 *  
-		 *  A. data 추가
+		 *  A. data 삭제
 		 *  
 		 *  	1 / 아메리카노 / 3000 / 빽다방
 		 *  	2 / 카푸치노 / 3500 / 바나프레소
 		 *  	3 / 아인슈페너 / 4000 / 스타벅스
+		 *  	4 / 레이디 그레이 / 5000 / 트와이닝
 		 *  
-		 *  새로운 메뉴를 추가(레이디 그레이 / 5000 / 트와이닝)
-		 *  단, num는 cafe_seq로 추가.
+		 *  2번 데이터 삭제
+		 *  
 		 * */
 		
-		//
 		Connection conn = null;
-		PreparedStatement pstmt = null;  
+		PreparedStatement pstmt = null;
 		
-		int num=0;
-		String menu="레이디그레이";
-		int price=5000;
-		String cafename="트와이닝";
-				
+		int num = 2;
+		
 		try {
 			conn = new DBConnect().getConn();
 			
-			String sql="insert into cafe"
-					+" values( cafe_seq.NEXTVAL, ?, ?, ?)";
-			
+			String sql = "delete from cafe where num=?";
 			pstmt=conn.prepareStatement(sql);
-			
-			pstmt.setString(1, menu);
-			pstmt.setInt(2, price);
-			pstmt.setString(3, cafename);
-			
+			pstmt.setInt(1, num);
 			pstmt.executeUpdate();
 			
-			System.out.println("메뉴를 추가 했습니다.");
+			System.out.println("삭제 완료");
 			
 		}catch(Exception e) {
 			e.printStackTrace();
---
+++ Step19_JDBC/src/test/cafe/MainClass05.java
@@ -0,0 +1,26 @@
+package test.cafe;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import test.util.DBConnect;
+
+public class MainClass05 {
+	public static void main(String[] args) {
+		/*
+		 *  [ cafe: num(pk) / menu / price / cafename ]
+		 *  
+		 *  A. 
+		 *  
+		 *  	1 / 아메리카노 / 3000 / 빽다방
+		 *  	3 / 아인슈페너 / 4000 / 스타벅스
+		 *  	4 / 레이디 그레이 / 5000 / 트와이닝
+		 *  
+		 *  데이터 3개를 추가하고 모든 메뉴를
+		 *  num기준으로 오름차순 정렬해서 List<CafeDto> 객체에 담기
+		 *  
+		 * */
+		
+		
+	}//main
+}//MainClass02
